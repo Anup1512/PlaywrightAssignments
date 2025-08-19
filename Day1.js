7ce2d1019df02661291da909ce481167be112b94
Anup1512 <anupkmr72@gmail.com>
2025-08-20 04:40:19 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Day1.js
@@ -0,0 +1,23 @@
+const city = "Chennai"
+console.log("I live in <"+city+">")
+console.log(typeof city)
+
+ 
+var marks=85
+console.log(marks+10)
+console.log(typeof marks)
+
+ let isweekend = true
+ if(isweekend)
+ {
+    console.log("Relax")
+ }
+ else
+ {
+    console.log("work")
+ }
+ console.log(typeof isweekend)
+
+
+var futuregoals
+ console.log(typeof futuregoals)

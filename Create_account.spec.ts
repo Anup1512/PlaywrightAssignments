e6ed6061dc832023f925792a4855aca942896b1f
Anup1512 <anupkmr72@gmail.com>
2025-09-06 07:02:02 +0530

    Add files via upload
    Uploaded Day 5 assignments

---
+++ Create_account.spec.ts
@@ -0,0 +1,73 @@
+import { expect } from "@playwright/test"
+import test,{ chromium } from '@playwright/test'
+
+
+test('Creating a salesforce account',async({page})=>{
+
+
+    await page.goto("https://login.salesforce.com/")
+
+ await page.waitForTimeout(2000)
+
+    await page.getByLabel('Username').fill("anupkumar.t543@agentforce.com")
+    await page.getByLabel('Password').fill("Test@salesforce2025")
+    await page.locator("#Login").click()
+
+       await page.waitForTimeout(12000)
+
+       // To verify the page title and Url
+
+   //const title = await page.locator("//title[contains(text(),'Salesforce')]")
+   await expect(page).toHaveTitle('Home | Salesforce')
+   const title =await page.title()
+   console.log(title)
+
+   await expect(page).toHaveURL('https://orgfarm-503c21e0be-dev-ed.develop.lightning.force.com/lightning/page/home')
+     console.log(page.url())
+
+         await page.locator("//div[@class='appLauncher slds-context-bar__icon-action']").click()
+         await page.waitForTimeout(5000)
+         await page.getByLabel('View All Applications').click()
+         await page.getByPlaceholder('Search apps or items...').fill('Service')
+         await page.locator("(//p//mark[contains(text(),'Service')])[1]").click()
+         await page.locator("//a[@title='Accounts']").click()
+         await page.getByRole('button',{name:'New'}).click() 
+       //  await page.waitForTimeout(120000)
+         
+         await expect(page.locator("//div//h2[text()='New Account']")).toBeVisible({ timeout: 120000 })
+
+         //unable to fill account name as element is not getting displayed
+         await page.locator('#input-646').fill("ABS consultants")
+         //await page.getByLabel("Account Name").fill("ABS consultants");
+
+         await page.waitForTimeout(2000)
+
+
+         await page.locator("//button[text()='Save']").click()
+          
+          
+         
+         
+        
+          
+
+
+
+         
+
+      
+})
+
+
+   
+      
+    
+
+
+
+
+
+
+
+
+
